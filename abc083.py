ba9b48358aa0840f0719378948c549a12713a880
yuizho <yuizho3@gmail.com>
2019-02-10 11:09:45 +0900

    improve abc083 logic

--- abc083.py
+++ abc083.py
@@ -1,9 +1,12 @@
 end, min_num, max_num = map(int, input().split())
 result = 0
 for n in range(1, end+1):
-    tmp = 0
-    for s in str(n):
-        tmp += int(s)
-    if tmp >= min_num and tmp <= max_num:
+    #tmp = 0
+    #for s in str(n):
+    #    tmp += int(s)
+    #if tmp >= min_num and tmp <= max_num:
+    #    result += n
+    x = sum(map(int, str(n)))
+    if min_num <= x <= max_num:
         result += n
 print(result)
