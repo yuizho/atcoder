acd0cd6e1e13e8b6131273084bd1fa3810576e12
yuizho <yuizho3@gmail.com>
2019-02-11 22:21:58 +0900

    modify abc086c
    to pass all cases

--- abc086_c.py
+++ abc086_c.py
@@ -1,29 +1,19 @@
 N = int(input())
 
-
-def canReach(start, T, x, y):
-    target = (x, y)
-    tmp = start
-    for t in range(T):
-        x_diff = target[0] - tmp[0]
-        y_diff = target[1] - tmp[1]
-        if abs(x_diff) >= abs(y_diff):
-            tmp = (tmp[0]-1, tmp[1]) if x_diff < 0 else (tmp[0]+1, tmp[1])
-        else:
-            tmp = (tmp[0], tmp[1]-1) if y_diff < 0 else (tmp[0], tmp[1]+1)
-    return tmp == target
-
-
-start = (0, 0)
+start_x, start_y = (0, 0)
 isReachAble = False
-T = 0
 for n in range(N):
     t, x, y = map(int, input().split())
-    isReachAble = canReach(start, t-T, x, y)
-    if (not isReachAble):
+    dist = abs(x-start_x) + abs(y-start_y)
+    if (dist > t):
         print('No')
         exit()
-    start = (x, y)
-    T += t
+    isTimeEven = t % 2 == 0
+    isDestEven = (x + y) % 2 == 0
+    if (isTimeEven and isDestEven) or (not isTimeEven and not isDestEven):
+        isReachAble = True
+    else:
+        print('No')
+        exit()
 
 print('Yes' if isReachAble else 'No')
