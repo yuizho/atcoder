e7cc065228360161401c57663e1e8bc8ffa8eafd
yuizho <yuizho3@gmail.com>
2019-05-25 12:36:01 +0900

    add abc087b written with kotlin

---
+++ kotlin/src/main/kotlin/io/github/yuizho/abc87/b/Main.kt
@@ -0,0 +1,23 @@
+package io.github.yuizho.abc87.b
+
+fun main(args: Array<String>) {
+    val fiveHandledYenCnt = readLine()!!.toInt()
+    val handledYenCnt = readLine()!!.toInt()
+    val fiftyYenCnt = readLine()!!.toInt()
+    val amount = readLine()!!.toInt()
+
+    var answer = 0
+    for (fh in 0..fiveHandledYenCnt) {
+        var fhAmount = fh * 500
+        for(h in 0..handledYenCnt) {
+            var hAmount = h * 100
+            for (f in 0..fiftyYenCnt) {
+                var fAmount = f * 50
+                if (fhAmount + hAmount + fAmount == amount) {
+                    answer += 1
+                }
+            }
+        }
+    }
+    print(answer)
+}
