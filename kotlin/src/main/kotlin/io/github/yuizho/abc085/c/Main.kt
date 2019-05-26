f9b653b25a0cc95cd37c86573b123c9800f490e2
yuizho <yuizho3@gmail.com>
2019-05-26 14:30:04 +0900

    refactoring abc085c written with kotlin

--- kotlin/src/main/kotlin/io/github/yuizho/abc085/c/Main.kt
+++ kotlin/src/main/kotlin/io/github/yuizho/abc085/c/Main.kt
@@ -1,15 +1,15 @@
 package io.github.yuizho.abc085.c
 
- fun main(args: Array<String>) {
+fun main(args: Array<String>) {
     val (N, value) = readLine()!!.split(" ").map(String::toInt)
     for (n10000 in 0..N) {
         for (n5000 in 0..(N - n10000)) {
             val n1000 = N - n10000 - n5000
             if (n10000 * 10000 + n5000 * 5000 + n1000 * 1000 == value) {
-                print(listOf(n10000, n5000, n1000).joinToString(" "))
+                print("$n10000 $n5000 $n1000")
                 return
             }
         }
     }
-    print(listOf(-1, -1, -1).joinToString(" "))
+    print("-1 -1 -1")
 }
