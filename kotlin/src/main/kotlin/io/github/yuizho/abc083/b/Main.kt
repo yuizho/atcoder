e7f7c1f7fd8e5907fec8bec47d39b263fc6cfa31
yuizho <yuizho3@gmail.com>
2019-05-26 11:43:01 +0900

    modify abc083.b to be more kotlinized

--- kotlin/src/main/kotlin/io/github/yuizho/abc083/b/Main.kt
+++ kotlin/src/main/kotlin/io/github/yuizho/abc083/b/Main.kt
@@ -2,10 +2,9 @@ package io.github.yuizho.abc083.b
 
 fun main(args: Array<String>) {
     val (max, left, right) = readLine()!!.split(" ").map(String::toInt)
-    var total = 0
-    for (num in 1..max) {
-        val subtotal = num.toString().sumBy { it.toString().toInt() }
-        if (subtotal in left..right) total += num
-    }
+    val total = (1..max)
+        // a in left...right is left <= a && a <= right
+        .filter { it.toString().sumBy(Character::getNumericValue) in left..right }
+        .sum()
     print(total)
 }
