2544095c3913548a4cff90c3d0b618a47a685f0f
yuizho <yuizho3@gmail.com>
2019-06-04 21:43:01 +0900

    refactor abc086c written by kotlin

--- kotlin/src/main/kotlin/io/github/yuizho/abc086/c/Main.kt
+++ kotlin/src/main/kotlin/io/github/yuizho/abc086/c/Main.kt
@@ -12,14 +12,14 @@ fun main(args: Array<String>) {
     var result = "Yes"
     for (e in eachTern) {
         val timeDiff = e[0] - prevTern[0]
-        val xDiff = Math.abs(e[1] - prevTern[1])
-        val yDiff = Math.abs(e[2] - prevTern[2])
-        if (timeDiff < (xDiff + yDiff)) {
+        val diff
+                = Math.abs(e[1] - prevTern[1]) + Math.abs(e[2] - prevTern[2])
+        if (timeDiff < diff) {
             result = "No"
             break
         }
-        if ((timeDiff % 2 != 0 && (xDiff + yDiff) % 2 == 0) ||
-            (timeDiff % 2 == 0 && (xDiff + yDiff) % 2 != 0)) {
+        if ((timeDiff % 2 != 0 && diff % 2 == 0) ||
+            (timeDiff % 2 == 0 && diff % 2 != 0)) {
             result = "No"
             break
         }
