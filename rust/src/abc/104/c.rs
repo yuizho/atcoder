f3330ccf037899e87aa3c594199667b2fe2b79c7
yuizho <yuizho3@gmail.com>
2019-10-08 01:15:32 +0900

    modify the code for abc 104c written in rust (still incollect おしい！)

--- rust/src/abc/104/c.rs
+++ rust/src/abc/104/c.rs
@@ -10,7 +10,7 @@ fn main() {
     let mut iter = buf.split_whitespace();
     let problem_counts: usize = iter.next().unwrap().parse().unwrap();
     let target_score: usize = iter.next().unwrap().parse().unwrap();
-    let mut bornases: Vec<(usize, usize, usize)> = (0..problem_counts)
+    let bornases: Vec<(usize, usize, usize)> = (0..problem_counts)
         .enumerate()
         .map(|(i, _)| {
             (
@@ -22,57 +22,61 @@ fn main() {
         .map(|x| (x.0, x.1, x.0 * x.1 + x.2))
         .collect();
 
-    println!("{:?}", bornases);
+    #[derive(Clone, Debug)]
+    struct Pattern {
+        attempts: usize,
+        score: usize,
+        another_list: Vec<(usize, usize)>,
+    }
 
     // 各行を選ぶ選ばないパターンを作る
     let mut patterns = Vec::new();
     // 2の3乗の組み合わせを作成
     for i in 0..2usize.pow(problem_counts as u32) {
-        let mut row = (
-            0,
-            0,
-            (
+        let mut row = Pattern {
+            attempts: 0,
+            score: 0,
+            another_list: vec![(
                 bornases[problem_counts - 1].0,
                 bornases[problem_counts - 1].1,
-            ),
-        );
+            )],
+        };
         // ビット演算する際の最大桁が1桁目に来るとこで
         // シフトできれば良いので、number.len()-1までloop
+        let anothers: Vec<(usize, usize)> = (0..problem_counts)
+            .filter(|j| ((i >> j) & 1) != 1)
+            .map(|j| (bornases[j].0, bornases[j].1))
+            .collect();
         for j in 0..problem_counts {
             // j桁右シフトして最初のbitが1かチェック
             if ((i >> j) & 1) == 1 {
-                let another_score = if j == problem_counts - 1 {
-                    (
-                        bornases[problem_counts - 2].0,
-                        bornases[problem_counts - 2].1,
-                    )
-                } else {
-                    (
-                        bornases[problem_counts - 1].0,
-                        bornases[problem_counts - 1].1,
-                    )
+                row = Pattern {
+                    attempts: row.attempts + bornases[j].1,
+                    score: row.score + bornases[j].2,
+                    another_list: anothers.clone(),
                 };
-                row = (row.0 + bornases[j].1, row.1 + bornases[j].2, another_score);
             }
         }
         patterns.push(row);
     }
     patterns = patterns[0..patterns.len() - 1].to_vec();
 
-    println!("{:?}", patterns);
-
     let mut result = 10000000;
     for pattern in &patterns {
         let mut tmp_count = 0;
-        if pattern.1 >= target_score {
-            tmp_count = pattern.0;
+        if pattern.score >= target_score {
+            tmp_count = pattern.attempts;
         } else {
-            let mut current_score = pattern.1;
-            tmp_count = pattern.0;
-            for sub in 0..(pattern.2).1 {
-                if current_score < target_score {
-                    tmp_count += 1;
-                    current_score += (pattern.2).0;
+            let mut current_score = pattern.score;
+            tmp_count = pattern.attempts;
+            'outer: for anothers in pattern.another_list.iter().rev() {
+                for _ in 0..anothers.1 - 1 {
+                    if current_score < target_score {
+                        tmp_count += 1;
+                        current_score += anothers.0;
+                    } else {
+                        break 'outer;
+                    }
                 }
             }
             if current_score < target_score {
